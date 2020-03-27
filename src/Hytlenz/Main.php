5d071e05c6a7f27a82f739a84854a351d6e7311b
Lentou <33175224+Lentou@users.noreply.github.com>
2020-03-27 22:53:28 +0800

    Cleaning Code
    Tommorow i will do this, don't use now

--- src/Hytlenz/Main.php
+++ src/Hytlenz/Main.php
@@ -9,26 +9,68 @@ use pocketmine\{Player, Server};
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\utils\{Config, TextFormat as TF};
-
-use Hytlenz\SendTask;
+use pocketmine\utils\Config;
+use pocketmine\level\{Level, Position};
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\entity\{Effect, EffectInstance};
 
 class Main extends PluginBase implements Listener{
 
-private $prefix = "[GreetMsg]";
 
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getLogger()->info(TF::YELLOW . $this->prefix . TF::GREEN . " GreetMsg Enabled by Hytlenz");
 		$this->saveDefaultConfig();
-  }
+        }
 
-	public function onDisable() {
-		$this->getServer()->getLogger()->info(TF::YELLOW . $this->prefix . TF::RED . " GreetMsg Enabled by Hytlenz");
-	}
-	
 	public function onJoin(PlayerJoinEvent $event){
-		$joinTask = new SendTask($this, $event->getPlayer());
-		$this->getScheduler()->scheduleDelayedTask($joinTask, 20);
+                //Todo on config
+                // settings:
+                //   enable-voice-message: true
+                //.  enable-effects: true
+                //.  enable-title-and-subtitle: true
+                //.  enable-greet-form: false
+                //.  enable-welcome-message: true
+                //.  enable-event-packet: true
+                //. messages:
+                //.   voice-message: ""
+                //.   effects: BLINDNESS
+                //.   title: ""
+                //.   subtitle: ""
+                //.   welcome-message: ""
+                //.   event-packet: 1 [1 - Guardian Curse : 2 - Totem]
+                //. forms:
+                //.   form-type: Modal [Modal and Custom]
+                //.   form-title: "Test Form"
+                //.   form-content:
+                //.      - "Welcome To Server"
+                //.      - "Have a Nice Day!"
+                //.   form-button-modal:
+                //.      button1: ""
+                //.      button2: ""
+                $player = $event->getPlayer();  
+	        $title = $this->getConfig()->getNested("welcome.title");
+        	$title = str_replace("{player}", $this->player->getName(), $title);
+        
+        	$subtitle = $this->getConfig()->getNested("welcome.subtitle");
+        	$subtitle = str_replace("{player}", $this->player->getName(), $subtitle);
+        
+        	$voice = $this->getConfig()->getNested("welcome.voice_msg");
+        	$voice = str_replace("{player}", $this->player->getName(), $voice);
+        
+        	$welcome = $this->getConfig()->getNested("welcome.msg");
+        	$welcome = str_replace("{player}", $this->player->getName(), $welcome);
+        
+        	$player->addTitle($title, $subtitle);
+        	$player->sendTranslation($voice);
+        	$player->sendMessage($welcome);
+		
+                $pk = new LevelEventPacket();
+		$pk->evid = LevelEventPacket::EVENT_GUARDIAN_CURSE;
+		$pk->data = 0;
+		$pk->position = $this->player->asVector3();
+		$this->player->dataPacket($pk);
+
+		$effect = new EffectInstance(Effect::getEffect(Effect::BLINDNESS), 100, 0, false);
+                $this->player->addEffect($effect);
 	}
 }
