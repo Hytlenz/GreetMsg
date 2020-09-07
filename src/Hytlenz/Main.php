90273e51136c0f055a30f04767f9c00e846ae3aa
Lentou <33175224+Lentou@users.noreply.github.com>
2020-09-07 12:27:03 +0800

    version 2.0
    fix lines

--- src/Hytlenz/Main.php
+++ src/Hytlenz/Main.php
@@ -20,74 +20,73 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveResource("greetings.yml");
-	    $this->greet = new Config($this->getDataFolder() . "greetings.yml", Config::YAML);
-    }
+	    	$this->greet = new Config($this->getDataFolder() . "greetings.yml", Config::YAML);
+    	}
 
 	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
-		
-        if($this->greet->getNested("form-message.enable", true)){
-        	if($this->greet->getNested("form-message.form-type") == "Modal"){
-        	    $this->modalForm($player);
-            } else if ($this->greet->getNested("form-message.form-type") == "Simple"){
-            	$this->simpleForm($player);
-            }
-        } else {
-        	$this->greetings($player);
-        }   
-        
+        	if($this->greet->getNested("form-message.enable", true)){
+        		if($this->greet->getNested("form-message.form-type") == "Modal"){
+        	    		$this->modalForm($player);
+            		} else if ($this->greet->getNested("form-message.form-type") == "Simple"){
+            			$this->simpleForm($player);
+            		}
+        	} else {
+        		$this->greetings($player);
+        	}   
 	}
+
 	public function modalForm($player){
-        $form = new ModalForm(function (Player $player, $data) {
-            if ($data == 0) {
-                $this->greetings($player);
-            }
-            if ($data == 1) {
-                $this->greetings($player);
-            }
-        });
-        $form->setTitle($this->tags($player, $this->greet->getNested("form-message.form-title")));
-        $form->setContent($this->tags($player, $this->greet->getNested("form-message.content")));
-        $form->setButton1($this->tags($player, $this->greet->getNested("form-message.modal.button1")));
-        $form->setButton2($this->tags($player, $this->greet->getNested("form-message.modal.button2")));
-        $form->sendToPlayer($player);
+        	$form = new ModalForm(function (Player $player, $data) {
+            		if ($data == 0) {
+                		$this->greetings($player);
+            		} else {
+                		$this->greetings($player);
+            		}
+        	});
+        	$form->setTitle($this->tags($player, $this->greet->getNested("form-message.form-title")));
+        	$form->setContent(implode("\n", $this->tags($player, $this->greet->getNested("form-message.content"))));
+        	$form->setButton1($this->tags($player, $this->greet->getNested("form-message.modal.button1")));
+        	$form->setButton2($this->tags($player, $this->greet->getNested("form-message.modal.button2")));
+        	$form->sendToPlayer($player);
 	}
+
 	public function simpleForm($player){
 		$form = new SimpleForm(function (Player $player, $data) {
-            if ($data == 0) {
-                $this->greetings($player);
-            }
-            if ($data == 1) {
-            	$this->greetings($player);
-            }
-        });
-        $form->setTitle($this->tags($player, $this->greet->getNested("form-message.form-title")));
-        $form->setContent($this->tags($player, $this->greet->getNested("form-message.content")));
-        $form->addButton($this->tags($player, $this->greet->getNested("form-message.simple.button")), $this->greet->getNested("form-message.simple.image-type"), $this->greet->getNested("form-message.simple.image-path"));
-        $form->sendToPlayer($player);
+			if ($data == 0) {
+				$this->greetings($player);
+			} else {
+				$this->greetings($player);
+			}
+		});
+        	$form->setTitle($this->tags($player, $this->greet->getNested("form-message.form-title")));
+        	$form->setContent(implode("\n", $this->tags($player, $this->greet->getNested("form-message.content"))));
+        	$form->addButton($this->tags($player, $this->greet->getNested("form-message.simple.button")), $this->greet->getNested("form-message.simple.image-type"), $this->greet->getNested("form-message.simple.image-path"));
+        	$form->sendToPlayer($player);
 	}
+
 	public function greetings($player){
 		if($this->greet->getNested("welcome-message.enable", true)){
-			$player->sendMessage($this->tags($player, $this->greet->getNested("welcome-message.message")));
+			$player->sendMessage(implode("\n", $this->tags($player, $this->greet->getNested("welcome-message.message"))));
 		}
 		if($this->greet->getNested("header-message.enable", true)){
 			$player->addTitle($this->tags($player, $this->greet->getNested("header-message.title")), $this->tags($player, $this->greet->getNested("header-message.subtitle")));
 		}
 		if($this->greet->getNested("effect.enable", true)){
-				$effectID = Effect::getEffect((int) $this->greet->getNested("effect.side-effect"));
-				$duration = (int) $this->greet->getNested("effect.duration");
-				$amplifier = (int) $this->greet->getNested("effect.amplifier");
-				$visible = $this->greet->getNested("effect.visible");
-				$player->addEffect(new EffectInstance($effectID, $duration * 20, $amplifier, $visible));
-        }
-        if($this->greet->getNested("event-packet.enable", true)){
-        	    $pk = new LevelEventPacket();
-                $pk->evid = LevelEventPacket::EVENT_GUARDIAN_CURSE;
-                $pk->data = 0;
-                $pk->position = $player->asVector3();
-                $player->dataPacket($pk);
-        }      
-    }
+			$effectID = Effect::getEffect((int) $this->greet->getNested("effect.side-effect"));
+			$duration = (int) $this->greet->getNested("effect.duration");
+			$amplifier = (int) $this->greet->getNested("effect.amplifier");
+			$player->addEffect(new EffectInstance($effectID, $duration * 20, $amplifier, true));
+       		}
+        	if($this->greet->getNested("event-packet.enable", true)){
+        	    	$pk = new LevelEventPacket();
+                	$pk->evid = LevelEventPacket::EVENT_GUARDIAN_CURSE;
+                	$pk->data = 0;
+                	$pk->position = $player->asVector3();
+                	$player->dataPacket($pk);
+        	}      
+    	}
+
 	public function tags(Player $player, string $string) : string {
 		$string = str_replace("@player", $player->getName(), $string);
 		$string = str_replace("&", "§", $string);
